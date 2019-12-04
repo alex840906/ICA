0880b12d5ca87d22b7c01f5c7e0b3f67983217a9
fawcat0906 <fawcat0906@gmail.com>
2019-12-04 22:57:48 +0800

    1204

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <time.h>
+#include "object.h"
+#include "operator.h"
+#include "ICA_ARG.h"
+#include "fitness.h"
+
+
+int main()
+{
+    //vector<Spider> femaleSpiderList(femaleSpider);
+    // cout<<femaleSpiderList.size()<<endl;
+    // cout<<maleSpiderList.size()<<endl;
+    // cout<<femaleSpiderList[0].gender;
+    system("pause");
+    return 0;
+}
